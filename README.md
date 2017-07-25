46f503b523479aeeab799d88b3a212d4462f53e1
cuponthetop <cuponthetop@gmail.com>
2017-07-26 00:42:16 +0900

    remove unnecessary

--- README.md
+++
@@ -1,4 +0,0 @@
-# gachi_back
-가치 백엔드
-
-should create own lib/config/firebase-service-config.json
--- db-scripts/reset-tables.sql
+++
@@ -1,3 +0,0 @@
-USE gachi
-
-DROP TABLE IF EXISTS user, festival, genre, fav_genre, festival_genre,historyleader, request;
