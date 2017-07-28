6f0bcb5bf51fbcf92d0b5a53708b569543292b09
cuponthetop <cuponthetop@gmail.com>
2017-07-29 00:06:38 +0900

    add test-web

---
+++ test-web/chat.js
@@ -0,0 +1,10 @@
+var db = myFirebase.database();
+var room = db.ref('chat/room1');
+
+postButton.click(function () {
+  var msgUser = username;
+  var msgText = textInput.val();
+  room.push({ username: msgUser, text: msgText });
+
+  textInput.val("");
+});
---
+++ test-web/common.js
@@ -0,0 +1,14 @@
+var myFirebase = new Firebase('https://gachi-test.firebaseio.com/');
+var gachi = 'http://localhost:3003/api/v1';
+
+var emailInput = $('#email');
+var passwordInput = $('#password');
+var nicknameInput = $('#nickname');
+var textInput = $('#text');
+var postButton = $('#post');
+var registerButton = $('#register');
+var loginButton = $('#login');
+
+var username = null;
+postButton.hide();
+textInput.hide();
