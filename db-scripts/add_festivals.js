1703866e7045c75f572d5ea099fe9a9a031a2dea
cuponthetop <cuponthetop@gmail.com>
2017-08-05 16:52:34 +0900

    fix auxillary

--- db-scripts/add_festivals.js
+++ db-scripts/add_festivals.js
@@ -2,8 +2,8 @@ let rq = require('request-promise');
 let _ = require('lodash');
 
 const festivals = require('./festivals.1.json');
-// const GACHI_URL = 'http://cuponthetop.com/gachi/api/v1/festival';
-const GACHI_URL = 'http://localhost:3003/api/v1/festival';
+const GACHI_URL = 'http://cuponthetop.com/gachi/api/v1/festival';
+// const GACHI_URL = 'http://localhost:3003/api/v1/festival';
 
 function processGenre(genre) {
   return genre.replace(/R&B/g, '알앤비').split('/');
