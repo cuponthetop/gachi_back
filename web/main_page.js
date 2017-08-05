1c892bdc5abfd0368967f64fa043467326ece1e8
김정윤 <skyepodium@gmail.com>
2017-08-05 23:50:06 +0900

    08.05 프론트 수정

--- web/main_page.js
+++ web/main_page.js
@@ -1,14 +1,25 @@
 var express = require('express');
 var engine = require('ejs-locals');
 var app = express();
+var Sequelize = require('sequelize');
 
+const syncDB = require(__dirname + '/sync-db')
+
+//ejs 템플릿 엔진
 app.engine('ejs', engine);
 app.set('views', __dirname + '/view/main_page');
 app.set('view engine', 'ejs');
 
+//정적파일 위치
 app.use(express.static( __dirname + '/static'));
+
+/*
+syncDB.then(_=> {
+  console.log('sync database');
+})
+*/
+//라우팅및 서버실행
 app.get('/', function(req, res){
   res.render('main_page');
 });
-
 app.listen(3030);
