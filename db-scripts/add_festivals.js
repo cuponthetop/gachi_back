87f928c50fdc704adbd9ca2592e22032a74d459b
cuponthetop <cuponthetop@gmail.com>
2017-08-05 16:09:16 +0900

    change sample data

--- db-scripts/add_festivals.js
+++ db-scripts/add_festivals.js
@@ -1,9 +1,9 @@
 let rq = require('request-promise');
 let _ = require('lodash');
 
-const festivals = require('./festivals.json');
-const GACHI_URL = 'http://cuponthetop.com/gachi/api/v1/festival';
-// const GACHI_URL = 'http://localhost:3003/api/v1/festival';
+const festivals = require('./festivals.1.json');
+// const GACHI_URL = 'http://cuponthetop.com/gachi/api/v1/festival';
+const GACHI_URL = 'http://localhost:3003/api/v1/festival';
 
 function processGenre(genre) {
   return genre.replace(/R&B/g, '알앤비').split('/');
