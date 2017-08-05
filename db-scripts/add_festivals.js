3e985a9bde0c5921d823c4965d1b8be966663444
cuponthetop <cuponthetop@gmail.com>
2017-08-05 14:46:36 +0900

    add some more data

--- db-scripts/add_festivals.js
+++ db-scripts/add_festivals.js
@@ -2,6 +2,7 @@ let rq = require('request-promise');
 let _ = require('lodash');
 
 const festivals = require('./festivals.1.json');
+// const GACHI_URL = 'http://cuponthetop.com/gachi/api/v1/festival';
 const GACHI_URL = 'http://localhost:3003/api/v1/festival';
 
 let promises = _.map(festivals, function (el) {
