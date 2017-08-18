123720ffa1d3d0a3c1fa16e9ca5742607b344b2f
cuponthetop <cuponthetop@gmail.com>
2017-08-19 02:57:58 +0900

    fix main page to use vue component

--- web/controller/all.js
+++ web/controller/all.js
@@ -1,65 +1,32 @@
 var request = require('request');
-var http  = require('http');
-var moment = require('moment');
+var http = require('http');
 
-var index_data;
-
-var festData = function(callback){
-  var options = {
-      url: 'http://cuponthetop.com/gachi/api/v1/festival/list?page=1&type=default',
-      method: 'GET',
-  };
-  request(options, function(err, res, body) {
-      index_data = JSON.parse(res.body);
-      //index_data = res.body;
-      callback();
-  });
-}
-
-var index = function(req, res){
- festData(function() {
-   res.render('main_page',{'festival':index_data, moment:moment});
- });
+var index = function (req, res) {
+  res.render('main_page');
 };
 
-const search = function(req, res){
-  var term=req.query.term;
+const search = function (req, res) {
+  var term = req.query.term;
 
-  res.render('../search/search',{term: term});
+  res.render('../search/search', { term: term });
 };
 
-const login = function(req, res){
- res.render('../login/login');
+const login = function (req, res) {
+  res.render('../login/login');
 };
 
-const signup = function(req, res){
- res.render('../signup/signup');
+const signup = function (req, res) {
+  res.render('../signup/signup');
 };
 
-const info_slide = function(req, res){
+const info_slide = function (req, res) {
   res.render('../info_slide/info_slide');
 }
 
-var detail_data;
-var fid="";
+const detail = function (req, res) {
+  var fid = req.params.fid;
 
-var detailData = function(callback){
-  var options = {
-      url: 'http://cuponthetop.com/gachi/api/v1/festival/'+fid,
-      method: 'GET',
-  };
-  request(options, function(err, res, body) {
-      detail_data = JSON.parse(res.body);
-      callback();
-  });
-}
-
-const detail = function(req, res){
-  fid = req.params.fid;
-
-  detailData(function() {
-    res.render('../detail/detail',{'festival': detail_data, 'moment':moment});
-  });
+  res.render('../detail/detail', { 'fid': fid });
 };
 
 
