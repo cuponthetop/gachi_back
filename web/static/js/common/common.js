1e9820a9141de1b24620a809da6870d4ee852a20
cuponthetop <cuponthetop@gmail.com>
2017-08-26 14:47:07 +0900

    fix server url

--- web/static/js/common/common.js
+++ web/static/js/common/common.js
@@ -1,8 +1,8 @@
 var gachi = (function (gachi, $) {
 
   var $ = $;
-  // gachi.GACHI_URL = "http://cuponthetop.com/gachi/api/";
-  gachi.GACHI_URL = "http://localhost:3003/api/";
+  gachi.GACHI_URL = "http://cuponthetop.com/gachi/api/";
+  // gachi.GACHI_URL = "http://localhost:3003/api/";
 
   gachi.authHeader = function (token) {
     return { authorization: 'Bearer ' + token };
